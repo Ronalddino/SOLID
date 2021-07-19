dab21c77ae19037d8a13a4827a32ea2739cf7497
Ronalddino <ronnaldgamez@gmail.com>
2021-07-18 19:50:12 -0500

    commit02

--- PAC.py
+++ PAC.py
@@ -1,14 +1,14 @@
-from abc import ABC, abstractmethod
-print(ABC)
-class Coche:
-
+class Discount:
+    # obtencion variables
     def __init__(self, customer, price):
       self.customer = customer
       self.price = price
-
+    #Descuento 
     def get_discount(self):
       return self.price * 0.2
 
 class VIPDiscount(Discount):
+    #obtencion variables 
     def get_discount(self):
+      #descuento sobre funcion
       return super().get_discount() * 2
--- SRP.py
+++ SRP.py
@@ -1,13 +1,17 @@
 
 class Coche:
+
+    #Obtencion variables
     def __init__(self, marca : str):
         self.marca = marca
         print(marca)
-        
+    # obtencion marca
     def get_marca(self):
         print("La marca es"+" "+self.marca)
 
+#Escritura en base de datos 
 class CocheDB:
+    
     def get_auto(self) -> Coche:
         pass
 
